daab5df7c5ad94670969cb4c7f4fe9410ceaecdd
Boris Schauer <me@bschauer.de>
2016-08-17 11:53:14 +0200

    [TASK] TS

--- Configuration/PageTS/PageTS.ts
+++ Configuration/PageTS/PageTS.ts
@@ -1,1 +1,24 @@
 ## Page TS ##
+
+## Headlines ##
+TCEFORM.tt_content.header_layout.altLabels {
+    1 = Headline H1
+    2 = Headline H2
+    3 = Headline H3
+    4 = Headline H4
+    5 = Headline H5
+    6 = Headline H6
+}
+## Headlines - END ##
+
+## Disable Copy Flag ##
+TCEMAIN.table.pages {
+    disablePrependAtCopy = 1
+    disableHideAtCopy = 1
+}
+
+TCEMAIN.table.tt_content {
+    disablePrependAtCopy = 1
+    disableHideAtCopy = 1
+}
+## Disable Copy Flag - END ##
