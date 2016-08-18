51b8957c8371d58a0257b42925b872a734cb17ef
Boris Schauer <me@bschauer.de>
2016-08-18 11:42:51 +0200

    [TASK] Boostrap RTE from
    https://github.com/benjaminkott/bootstrap_package

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,7 +14,14 @@ if (!defined('TYPO3_MODE')) {
     . $_EXTKEY . '/Configuration/PageTS/Backend_Layouts.ts">'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:'
-    . $_EXTKEY . '/Configuration/PageTS/BootstrapRTE.ts">'
-);
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+}
+
+if (!$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['disablePageTsRTE']) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/BootstrapRTE.ts">');
+}
+
+if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+}
