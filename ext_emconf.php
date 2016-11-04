aea86bf77aaa52b57dafa97e8b34d5f2fa35e1d9
Harry Glatz <glatz@analog.de>
2016-11-04 06:32:53 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.9-8.99.99',
-            'vhs' => '*'
+            'typo3' => '7.6.9-8.99.99'
         ),
         'conflicts' => array(
             'fluidpages' => '*',
