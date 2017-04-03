657fcb789edd90247132ca942d52574d0bb23b6a
Boris Schauer <me@bschauer.de>
2017-04-03 21:31:41 +0200

    [TASK] new init script

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@
 You can use the `start.sh` script to clone `startpilot` as extension with any name. Simply use the following shell command in the root of your project where your composer.json file is located. Replace "myext" at the end of the line with the name of the extension you want to create. 
 
 ```
-curl https://raw.githubusercontent.com/misterboe/startpilot/master/start.sh > start.sh && sh start.sh ./web/typo3conf/ext/myext
+curl https://raw.githubusercontent.com/misterboe/startpilot/master/start.sh > start.sh && sh start.sh --extname myprovider --vendor myvendor --package mypackage
 ```
 This script is tested on MAC OSX.
 
--- start.sh
+++ start.sh
@@ -1,25 +1,60 @@
 #! /bin/bash
 
-DIR="$1"
+while [[ $# -gt 1 ]]
+do
+key="$1"
 
-if [ $# -ne 1 ]
-then
-	echo "Usage: $0 {new extension key}"
-	exit 1
-fi
+case $key in
+    -e|--extname)
+    EXTNAME="$2"
+    shift # past argument
+    ;;
+    -v|--vendor)
+    VENDOR="$2"
+    shift # past argument
+    ;;
+    -p|--package)
+    PACKAGE="$2"
+    shift # past argument
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+path=($(find . -name "typo3conf")/ext)
+DIR=${path}/$EXTNAME
+
+vendor=`echo ${VENDOR:0:1} | tr  '[a-z]' '[A-Z]'`${VENDOR:1}
+package=`echo ${PACKAGE:0:1} | tr  '[a-z]' '[A-Z]'`${PACKAGE:1}
+
+echo "Directory: $DIR"
+echo "Vendor: $vendor"
+echo "Package: $PACKAGE_upper"
 
 if [ -d "$DIR" ]
 then
 	echo "Directory/extension '$DIR' exists already!"
 else
-    echo "Please enter Vendor Name (first character uppercase): "
-    read vendor
+    if [ -z ${vendor} ];
+    then
+        echo "Please enter Vendor Name (first character uppercase): "
+        read VENDOR
+        vendor=`echo ${VENDOR:0:1} | tr  '[a-z]' '[A-Z]'`${VENDOR:1}
+    fi
     echo "Your Vendor will be: $vendor"
     echo
-    echo "Please enter Package Name (first character uppercase): "
-    read package
+    if [ -z ${package} ];
+    then
+        echo "Please enter Package Name (first character uppercase): "
+        read PACKAGE
+        package=`echo ${PACKAGE:0:1} | tr  '[a-z]' '[A-Z]'`${PACKAGE:1}
+    fi
     echo "Your Package will be: $package"
     echo
+    echo ${DIR}
 	git clone https://github.com/misterboe/startpilot.git $DIR --depth=1
 	echo "$DIR created."
 	cd $DIR && rm -rf .git && grep -rl "startpilot" ./* -R | xargs sed -i '' "s/startpilot/${PWD##*/}/g" && grep -rl "Startpilot" ./* -R | xargs sed -i '' "s/Startpilot/${PWD##*/}/"
