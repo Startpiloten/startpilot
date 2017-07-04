7cb4604c2d23b67bf4b14010d2dc8ec740e19ff1
Miro Olma <olma@medienagenten.de>
2017-07-04 16:47:18 +0200

    [update] clean tasks, add todo delete file on watcher callback…

--- Resources/Build/Gulp/clean.js
+++ Resources/Build/Gulp/clean.js
@@ -10,31 +10,31 @@ const packageJson = require('../package.json');
 gulp.task('clean:scripts', function () {
     'use strict';
     return gulp.src(packageJson.config.path.dest + '/JavaScript/**/*', {read: false})
-        .pipe(rimraf({force: true}));
+        .pipe(rimraf({ force: true })).resume();
 });
 
 gulp.task('clean:styles', function () {
     'use strict';
     return gulp.src(packageJson.config.path.dest + '/Scss/**/*', {read: false})
-        .pipe(rimraf({force: true}));
+        .pipe(rimraf({ force: true })).resume();
 });
 
 gulp.task('clean:fonts', function () {
     'use strict';
     return gulp.src(packageJson.config.path.dest + '/Fonts/**/*', {read: false})
-        .pipe(rimraf({force: true}));
+        .pipe(rimraf({ force: true })).resume();
 });
 
 gulp.task('clean:images', function () {
     'use strict';
     return gulp.src(packageJson.config.path.dest + '/Images/**/*', {read: false})
-        .pipe(rimraf({force: true}));
+        .pipe(rimraf({ force: true })).resume();
 });
 
 gulp.task('clean:misc', function () {
     'use strict';
     return gulp.src(packageJson.config.path.dest + '/Misc/**/*', {read: false})
-        .pipe(rimraf({force: true}));
+        .pipe(rimraf({ force: true })).resume();
 });
 
 gulp.task('clean', gulp.series(['clean:scripts', 'clean:styles', 'clean:fonts', 'clean:images', 'clean:misc']));
--- Resources/Build/gulpfile.js
+++ Resources/Build/gulpfile.js
@@ -2,7 +2,13 @@
  * Gulp
  */
 const gulp = require('gulp');
+/*const del = require('del');
+const path = require('path');
+const gutil = require('gulp-util');
+const log = require('gutil-color-log')*/
 const packageJson = require('./package.json');
+
+
 require('./Gulp/styles');
 require('./Gulp/images');
 require('./Gulp/scripts');
@@ -19,7 +25,25 @@ gulp.task('watch', function () {
     gulp.watch(packageJson.config.path.src + '/Fonts/**/*', gulp.series('fonts'));
     gulp.watch(packageJson.config.path.src + '/Misc/**/*', gulp.series('misc'));
     gulp.watch([packageJson.config.path.src + '/JavaScript/**/*.js', packageJson.config.path.src + '/JavaScript/**/*.json'], gulp.series('javascript'));
+
+
+    /*
+        @TODO add watcher to delete files in dist…
+
+    miscWatcher.on('unlink', function(src) {
+        if(path.basename(src) != '.DS_Store') {
+            const file = src.replace('Assets/', '');
+            log('red', 'File ' + file + ' was removed.');
+
+            let destFilePath = path.relative(path.resolve(packageJson.config.path.dest), src);
+                destFilePath = destFilePath.replace('Build/Assets/', 'Resources/Public/')
+            log('blue', 'File ' + '../'+destFilePath + ' should be removed…');
+            //del.sync(destFilePath, { force: true });
+        }
+    });*/
 });
+
+
 gulp.task('build', gulp.parallel('clean', 'css', 'fonts', 'misc', 'image', 'javascript'));
 gulp.task('ci', gulp.parallel('css-lint', 'build'));
 gulp.task('default', gulp.series('build', gulp.parallel('watch')));
