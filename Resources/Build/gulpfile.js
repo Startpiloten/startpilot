b6db31472d317442ffb298ed4d112f521a64c494
Erik Jahn <mail@erikjahn.de>
2017-07-05 11:22:54 +0200

    [FIX] watcher: sync file deletion

--- Resources/Build/gulpfile.js
+++ Resources/Build/gulpfile.js
@@ -2,10 +2,10 @@
  * Gulp
  */
 const gulp = require('gulp');
-/*const del = require('del');
+const del = require('del');
 const path = require('path');
 const gutil = require('gulp-util');
-const log = require('gutil-color-log')*/
+const log = require('gutil-color-log')
 const packageJson = require('./package.json');
 
 
@@ -23,24 +23,24 @@ gulp.task('watch', function () {
     gulp.watch(packageJson.config.path.src + '/Scss/**/*.scss', gulp.series('css'));
     gulp.watch(packageJson.config.path.src + '/Images/**/*', gulp.series('image'));
     gulp.watch(packageJson.config.path.src + '/Fonts/**/*', gulp.series('fonts'));
-    gulp.watch(packageJson.config.path.src + '/Misc/**/*', gulp.series('misc'));
+    const miscWatcher = gulp.watch(packageJson.config.path.src + '/Misc/**/*', gulp.series('misc'));
     gulp.watch([packageJson.config.path.src + '/JavaScript/**/*.js', packageJson.config.path.src + '/JavaScript/**/*.json'], gulp.series('javascript'));
 
 
     /*
         @TODO add watcher to delete files in dist…
-
+    */
     miscWatcher.on('unlink', function(src) {
         if(path.basename(src) != '.DS_Store') {
             const file = src.replace('Assets/', '');
             log('red', 'File ' + file + ' was removed.');
 
             let destFilePath = path.relative(path.resolve(packageJson.config.path.dest), src);
-                destFilePath = destFilePath.replace('Build/Assets/', 'Resources/Public/')
-            log('blue', 'File ' + '../'+destFilePath + ' should be removed…');
-            //del.sync(destFilePath, { force: true });
+                destFilePath = '../' + destFilePath.replace('Build/Assets/', 'Resources/Public/')
+            log('blue', 'File ' + destFilePath + ' should be removed…');
+            del.sync(destFilePath, { force: true });
         }
-    });*/
+    });
 });
 
 
