03ad471516a4090ebda08d1f486aaf80d4874849
Boris Schauer <me@bschauer.de>
2018-02-23 13:47:07 +0100

    [TASK] RTE configuration
    • rename yaml file
    • remove table
    • remove hr

--- ext_localconf.php
+++ ext_localconf.php
@@ -20,7 +20,7 @@ if (TYPO3_MODE === 'BE') {
         ['source' => 'EXT:startpilot/ext_icon.svg']
     );
 
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:startpilot/Configuration/RTE/Bootstrap.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:startpilot/Configuration/RTE/CkEditor.yaml';
 
     /***************
      * Extension Manager Settings
