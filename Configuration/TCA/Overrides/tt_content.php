1df9830ad141d2f85f43cd977bd3f9a6b3e8bf54
Boris Schauer <me@bschauer.de>
2018-11-19 10:42:57 +0100

    [TASK] New way to init cTypes

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TCA']['tt_content']['palettes']['headers']['showitem'] = '
+header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel,
+';
--- Configuration/TCA/tt_content.php
+++ Configuration/TCA/tt_content.php
@@ -38,24 +38,3 @@ TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
     '--div--',
     'before'
 );
-
-/*
- * This variables are used to create the defaults in custom ctypes. (tt_content_textimage.php L:74:23)
- * */
-$showitem_default_01 = '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-                        ';
-$showitem_default_02 = '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
-                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-                            --palette--;;language,
-                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-                            --palette--;;hidden,
-                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-                            categories,
-                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-                            rowDescription,
-                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-                        ';
