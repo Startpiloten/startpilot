aa7c58900548c8467afe433931af384899d414b8
Boris Schauer <me@bschauer.de>
2019-10-31 10:04:09 +0100

    Fix Imagemin
    Error is "ArgumentError: Expected options.quality to be of type array but received type string".

--- Resources/Build/webpack.common.js
+++ Resources/Build/webpack.common.js
@@ -99,15 +99,20 @@ module.exports = {
                                 progressive: true,
                                 quality: 65
                             },
+                            // optipng.enabled: false will disable optipng
                             optipng: {
                                 enabled: false,
                             },
                             pngquant: {
-                                quality: '65-90',
+                                quality: [0.65, 0.90],
                                 speed: 4
                             },
                             gifsicle: {
                                 interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
                             }
                         }
                     }
@@ -144,15 +149,20 @@ module.exports = {
                     progressive: true,
                     quality: 65
                 },
+                // optipng.enabled: false will disable optipng
                 optipng: {
                     enabled: false,
                 },
                 pngquant: {
-                    quality: '65-90',
+                    quality: [0.65, 0.90],
                     speed: 4
                 },
                 gifsicle: {
                     interlaced: false,
+                },
+                // the webp option will enable WEBP
+                webp: {
+                    quality: 75
                 }
             }
         })
