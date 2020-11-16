4bac66687d5bfbbe62b56b050e39f4f64e24e3e3
Boris Schauer <me@bschauer.de>
2020-11-16 13:07:59 +0100

    BUGFIX publicPath of fonts

--- Resources/Build/webpack.common.js
+++ Resources/Build/webpack.common.js
@@ -88,7 +88,8 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 loader: 'file-loader',
                 options: {
-                    outputPath: 'Fonts/'
+                    outputPath: 'Fonts/',
+                    publicPath: './Fonts/',
                 }
             },
             {
